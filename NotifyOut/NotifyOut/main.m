a0194773bde77a6d36358dff068b8b28d13ecc5b
James Upjohn <jspartan250@gmail.com>
2019-03-15 19:12:37 +1300

    Add simple test notification

--- NotifyOut/NotifyOut/main.m
+++ NotifyOut/NotifyOut/main.m
@@ -7,7 +7,22 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <UserNotifications/UserNotifications.h>
+
+void runNotification(NSString *title, NSString *body) {
+    UNUserNotificationCenter *unc = [UNUserNotificationCenter currentNotificationCenter];
+    
+    UNMutableNotificationContent *notifContent = [UNMutableNotificationContent alloc];
+    notifContent.title = title;
+    notifContent.body = body;
+    
+    UNNotificationRequest *unr = [UNNotificationRequest requestWithIdentifier:@"Test" content:notifContent trigger:nil];
+    
+    [unc addNotificationRequest:unr withCompletionHandler:nil];
+}
 
 int main(int argc, const char * argv[]) {
+    runNotification(@"Test", @"Test body");
+    
     return NSApplicationMain(argc, argv);
 }
